686dae8d3276c05dff9ed8b50d44d517aad961a3
YSWickramasinghe <yasirusajith19970331@gmail.com>
2020-09-24 10:26:57 +0530

    Admin home activity

---
+++ app/src/main/java/com/example/eat_it/AdmHomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.eat_it;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class AdmHomeActivity extends AppCompatActivity {
+
+    private ImageButton FoodItemBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adm_home);
+
+        FoodItemBtn = (ImageButton) findViewById(R.id.HomeFoodimageButton);
+
+        FoodItemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdmHomeActivity.this, AdmCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
