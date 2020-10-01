24796d36efecd675a340821efb2441f98371fd1f
Nirmani <deeshaniathuraliya2@gmail.com>
2020-10-01 23:08:33 +0530

    IT19066530 Customer Registration Modified

--- app/src/main/java/com/example/eat_it/Register.java
+++ app/src/main/java/com/example/eat_it/Register.java
@@ -23,72 +23,79 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.HashMap;
 
 public class Register extends AppCompatActivity {
+    private EditText txtName,txtEmail,txtPhone,txtPassword;
+    private Button btnRegister;
 
-    EditText txtName,txtEmail,txtPhone,txtPassword;
-    Button btnRegister;
-    ProgressDialog lodingbar;
+
+    private ProgressDialog lodingbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
         txtName = (EditText) findViewById(R.id.editTextTextPersonName);
         txtEmail = (EditText) findViewById(R.id.editTextTextEmail);
         txtPhone = (EditText) findViewById(R.id.editTextTextPhone);
         txtPassword = (EditText) findViewById(R.id.editPassword);
+        lodingbar =new ProgressDialog(this);
 
-        btnRegister = (Button) findViewById(R.id.button);
+        btnRegister = (Button) findViewById(R.id.regiserBtn);
+        //cus = new Customer();
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //dbref = FirebaseDatabase.getInstance().getReference().child("Customer");
 
-                createCustomerAccount();
+                createAccount();
+
+
             }
         });
+
     }
+    private void createAccount(){
+        String name=txtName.getText().toString();
+        String email=txtEmail.getText().toString();
+        String phone=txtPhone.getText().toString();
+        String password=txtPassword.getText().toString();
 
-    private void createCustomerAccount() {
-
-        String name = txtName.getText().toString();
-        String email = txtEmail.getText().toString();
-        String phone = txtPhone.getText().toString();
-        String password = txtPassword.getText().toString();
-
-        if (TextUtils.isEmpty(name)) {
-            Toast.makeText(getApplicationContext(), "Please Enter the Full Name", Toast.LENGTH_SHORT).show();
-        } else if (TextUtils.isEmpty(email)) {
-            Toast.makeText(getApplicationContext(), "Please Enter the Email Address", Toast.LENGTH_SHORT).show();
-        } else if (TextUtils.isEmpty(phone)) {
-            Toast.makeText(getApplicationContext(), "Please Enter the Contact Number", Toast.LENGTH_SHORT).show();
-        } else if (TextUtils.isEmpty(password)) {
-            Toast.makeText(getApplicationContext(), "Please Enter the Password", Toast.LENGTH_SHORT).show();
-        } else {
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(getApplicationContext(),"Please Enter the Full Name",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(),"Please Enter the Email Address",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(phone)){
+            Toast.makeText(getApplicationContext(),"Please Enter the Contact Number",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password)){
+            Toast.makeText(getApplicationContext(),"Please Enter the Password",Toast.LENGTH_SHORT).show();
+        }
+        else{
             lodingbar.setTitle("Creating The Account");
             lodingbar.setMessage("Please Wait");
             lodingbar.setCanceledOnTouchOutside(false);
             lodingbar.show();
 
-            ValidateEmailAddress(name, email, phone, password);
+            ValidateEmailAddress(name,email,phone,password);
 
         }
     }
 
-    private void ValidateEmailAddress(final String name, final String email, final String phone, final String password) {
+    private  void ValidateEmailAddress(final String name, final String email, final String phone, final String password){
         final DatabaseReference dbref;
         dbref= FirebaseDatabase.getInstance().getReference();
 
-        dbref.addValueEventListener(new ValueEventListener() {
+        dbref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                if (!(snapshot.child("Customer").child(phone).exists())){
-
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!(dataSnapshot.child("Customer").child(phone).exists())){
                     HashMap<String,Object> customerDataMap =new HashMap<>();
                     customerDataMap.put("phone",phone);
                     customerDataMap.put("name",name);
                     customerDataMap.put("email",email);
+
 
                     customerDataMap.put("password",password);
 
@@ -98,7 +105,7 @@ public class Register extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if(task.isSuccessful())
                                     {
-                                        Toast.makeText(Register.this,"Your Account created Successfully",Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(Register.this,"Account created Successfully",Toast.LENGTH_SHORT).show();
                                         lodingbar.dismiss();
 
                                         Intent intent3 = new Intent(Register.this, Menu.class);
@@ -113,18 +120,17 @@ public class Register extends AppCompatActivity {
 
                 }
                 else{
-                    Toast.makeText(Register.this,"This "+phone+" number already exist",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Register.this,"this "+phone+" number already exist",Toast.LENGTH_SHORT).show();
                     lodingbar.dismiss();
 
                 }
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
-
     }
 
 }
